09b76de3a33b23253698a403e1ce1351d06eeee3
Semyon Radionov <simon.radionov@gmail.com>
2022-09-04 09:09:44 +0000

    upd

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ export function main() {
     console.log("Hello via Bun!");
     Bun.serve({
         fetch(req) {
-            return new Response("Hello via Bun.js!");
+            return new Response("Hello via Bun in codespace.js!");
         },
     });
 }
